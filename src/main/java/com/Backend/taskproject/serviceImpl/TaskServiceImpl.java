d3f770ed30ecadd2fd80eeaed96556647061e7c5
rajumanchikatla <rajumanchikatla90@gmail.com>
2024-03-28 15:04:40 +0530

    Logger msg's and Exceptions handled effectively

--- src/main/java/com/Backend/taskproject/serviceImpl/TaskServiceImpl.java
+++ src/main/java/com/Backend/taskproject/serviceImpl/TaskServiceImpl.java
@@ -3,6 +3,8 @@ package com.Backend.taskproject.serviceImpl;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,8 @@ import com.Backend.taskproject.service.TaskService;
 
 @Service
 public class TaskServiceImpl implements TaskService{
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(TaskServiceImpl.class);
 
 	@Autowired
 	private TaskRepository taskRepository;
@@ -32,17 +36,18 @@ public class TaskServiceImpl implements TaskService{
     }
 
     @Override
-    public Task gettaskbyid(long userid) {
+    public Task getTaskByid(long userid) {
         Optional<Task> taskOptional = taskRepository.findById(userid);
         return taskOptional.orElseThrow(() -> new UserNotFound("Task not found"));
     }
 
-    public List<Task> getalltasks(){
+    public List<Task> getAllTasks(){
        return taskRepository.findAll();
     }
 	
 
     public List<Task> getAllTasks(long userid){
+		LOGGER.info("Task Service get all tasks based on userid");
     	return taskRepository.findByUsers_Id(userid);
     }
     
